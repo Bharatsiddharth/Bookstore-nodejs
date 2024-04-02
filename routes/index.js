e510d00974a729fb4b5266cae70994658ea98dd4
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-04-03 00:47:48 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const Books = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,8 +12,14 @@ router.get('/create', function(req, res, next) {
   res.render('create');
 });
 
+router.post('/create', function(req, res, next) {
+  Books.push(req.body);
+  res.redirect("/readall")
+  console.log(Books);
+});
+
 router.get('/readall', function(req, res, next) {
-  res.render('Library');
+  res.render('Library', {books:Books});
 });
 
 router.get('/about', function(req, res, next) {
