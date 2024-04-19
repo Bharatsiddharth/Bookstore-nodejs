412b6612e126358cd106335aa53e78127da753b1
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-04-19 20:19:45 +0530

    multer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// const upload = require("utils/multer");
 const Book = require("../models/bookmodel");
 
-const Books = []
+// const Books = []
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,7 +25,8 @@ router.post('/create', async function(req, res, next) {
   //     }).catch((err) => res.send(err))
 
   try {
-    const newbook = new Book(req.body);
+    // const newbook = new Book(req.body);
+    const newbook = new Book({...req.body, image: req.file.filename})
     await newbook.save();
     res.redirect("/readall");
 } catch (error) {
@@ -34,20 +36,22 @@ router.post('/create', async function(req, res, next) {
   // console.log(Books);
 });
 
-router.get('/readall', function(req, res, next) {
-  Book.find() 
-    .then((book) => {
-      res.render('Library', {books:book});
-    }).catch((err) => res.send(err));
+router.get('/readall', async function(req, res, next) {
+    try {
+      const allbooks = await Book.find();
+      res.render("Library", {books: allbooks})
+    } catch (error) {
+      res.send(error);
+    }
 });
 
 router.get('/about', function(req, res, next) {
   res.render('About');
 });
 
-router.get('/delete/:idx', async function(req, res, next) {
+router.get('/delete/:id', async function(req, res, next) {
       try {
-        await Books.findByIdAndDelete(req.params.id);
+        await Book.findByIdAndDelete(req.params.id);
         res.redirect("/readall");
     } catch (error) {
         res.send(error);
@@ -60,11 +64,11 @@ router.get('/delete/:idx', async function(req, res, next) {
 router.get('/update/:idx',async function(req, res, next) {
 
       try {
-        const book = await Books.findById(req.params.id);
+        const book = await Book.findById(req.params.id);
         res.render("update", { book: book });
-    } catch (error) {
+      } catch (error) {
         res.send(error);
-    }
+      }
 
   // const i = req.params.idx;
   // const b = Books[req.params.idx]
